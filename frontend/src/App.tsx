e86eab15421b396af6f4fb085258f35b2146cec3
Daniel Esteban Bustos <123027736+DanielBustos342@users.noreply.github.com>
2024-12-18 13:02:13 -0300

    feat: :sparkles: material ui login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import "./reset.css";
 import ResponsiveAppBar from "./components/responsiveAppBar/ResponsiveAppBar";
 import ButtonSizes from "./components/buttonSizes/ButtonSizes";
+import SignIn from "./components/login/SigbIn";
 
 function App() {
   return (
     <>
-      <ResponsiveAppBar />
-      <ButtonSizes />
+      {/* <ResponsiveAppBar /> */}
+      {/* <ButtonSizes /> */}
+      <SignIn />
     </>
   );
 }
